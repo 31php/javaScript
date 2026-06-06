35ecbbed1e9b38d8dc23b98a30271077bd6cae0b
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-06-06 16:41:43 -0300

    exercícios com tipos primitivos finalizado

---
+++ exercícios/ex04.js
@@ -0,0 +1,12 @@
+//Dados primitivos
+ const nome = 'Phillip';// tipo string com aspas simples
+ const nome1 = "Gonçalves";// tipo string com aspas duplas
+ const nome2 = `Ferreira`;// tipo string com crase, onde também podemos usar o tamplate String
+ 
+ const num = 1;// tipo number onde não precisa de aspas para ser declarado
+ const num2 = 2.2;// tipo number
+
+ let nome; //undefined não aponta para lugar nenhum
+ let cor = null;//também não aponta pra lugar nenhum na memória do computador
+ 
+ const notaEscolar; //boolean tem dois valores, falso ou verdadeiro, ouseja, valores lógicos 
