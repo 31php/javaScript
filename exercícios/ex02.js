e1c8112f626fb08bb27a8eb333f2154eb4904bd6
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-06-06 15:17:10 -0300

    exercício com variáveis concluido

---
+++ exercícios/ex02.js
@@ -0,0 +1,10 @@
+//trabalhando com variavéis
+
+let nome = 'Hélio';
+let esposa = 'thaiane';
+
+console.log(nome, 'nascseu em 1985');
+console.log('em março de 2001,', nome,'conheçeu sua esposa');
+console.log(nome,'casou-se com sua esposa em 09/12/2016' );
+console.log(esposa,'teve 2 filhos com ', nome);
+console.log('seus filhos são: Arthur e Manuela');
