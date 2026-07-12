5919575e5e8dd38bd2bee6d917586cfa9c3536e9
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-07-12 11:29:31 -0300

    treinando funções

--- exercicioAula18.js
+++
@@ -1,8 +0,0 @@
-// funções
-
-const dobro = (n) =>  n * 2;
-
-const triplo = n => n * 3;
-
- console.log(dobro(2));
-console.log(triplo(7));
---
+++ exercícios/exercicioAula18.js
@@ -0,0 +1,9 @@
+// funções
+
+const dobro = function(n){
+    return n * 2;
+}
+console.log(dobro(36));
+console.log(dobro(92));
+console.log(dobro(37));
+console.log(dobro(39.768));
