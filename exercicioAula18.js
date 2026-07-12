d11453e8955e67942466ab07bd4f2a36ae21c382
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-07-12 09:02:52 -0300

    criando arrays

---
+++ exercicioAula18.js
@@ -0,0 +1,11 @@
+// funções
+
+function dobro(numero){
+    const resultado = numero * 2;
+    return resultado; 
+}
+dobro()
+console.log(dobro(5));
+console.log(dobro(10))
+console.log(dobro(15))
+console.log(dobro(20))
---
+++ exercícios/exercicoAula17.js
@@ -0,0 +1,4 @@
+// Arrays (vetores);
+
+const nome = [];
+const numeros = [];
