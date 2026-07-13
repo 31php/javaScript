309e6c39963c40d2500fdb20ddbf66b615223e4c
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-07-13 09:27:02 -0300

    Update exercicioAula18.js

--- exercícios/exercicioAula18.js
+++ exercícios/exercicioAula18.js
@@ -1,2 +1,11 @@
 // funções
 
+function soma(a, b){
+    const resultado = a + b;
+    return resultado;
+}
+//console.log(soma(5, 89.9876));
+const subtracao = function(n){
+    return n - 5;
+}
+console.log(subtracao(7.879));
