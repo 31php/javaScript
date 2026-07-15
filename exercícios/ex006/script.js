07309a78cc1f169d45ad80647e7089341f72222d
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-07-15 12:09:29 -0300

    treinando vetores(arrays)

--- exercícios/ex006/script.js
+++ exercícios/ex006/script.js
@@ -1,8 +1,8 @@
-window.alert('oi');
+window.alert('oi, bem-vindo ao javaScript ');
 
 let nome = window.prompt('Seu nome é?');
 
-
+window.confirm(`seu nome é ${nome}?`);
 
 
 
--- exercícios/ex011/exercicioAula17.js
+++ exercícios/ex011/exercicioAula17.js
@@ -1,4 +1,28 @@
 // Arrays (vetores);
 
-const nome = [];
-const numeros = [];
+const nome = ['phillip','thaiane'];
+
+nome[2] = 'manuela'; //adicionando elementos ao final do vetor(array)
+nome[3] = 'arthur';
+
+//console.log(nome);
+console.log(nome[1]); //acessando os elementos do vetor(array)
+
+nome.push('charlote'); // adicionando elementos ao final do vetor(array)
+console.log(nome);
+
+nome.unshift('teodora'); // adicionando elemento so início do vetor(array)
+console.log(nome);
+
+nome.pop('');// removendo elemento do final do vetor(array)
+console.log(nome);
+
+nome.shift(); // removendo elementos do início do vetor(array)
+console.log(nome);
+
+delete nome[0];
+console.log(nome); //removendo somente o valor do elemento, mas mantendo o índice do vetor(array)
+
+delete nome[2];
+console.log(nome);
+console.log(nome);
