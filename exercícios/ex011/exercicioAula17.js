2322d4211055fecf9b9300aaa8f6bbfd1324af3f
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-07-15 12:44:04 -0300

    treinando vetores

--- exercícios/ex011/exercicioAula17.js
+++ exercícios/ex011/exercicioAula17.js
@@ -1,28 +1,11 @@
 // Arrays (vetores);
 
-const nome = ['phillip','thaiane'];
+const nome = ['phillip','tahiane'];
 
-nome[2] = 'manuela'; //adicionando elementos ao final do vetor(array)
-nome[3] = 'arthur';
-
-//console.log(nome);
-console.log(nome[1]); //acessando os elementos do vetor(array)
-
-nome.push('charlote'); // adicionando elementos ao final do vetor(array)
 console.log(nome);
 
-nome.unshift('teodora'); // adicionando elemento so início do vetor(array)
+nome[2] = 'charlote'; // adicionando elementos ao vetor(array)
 console.log(nome);
 
-nome.pop('');// removendo elemento do final do vetor(array)
-console.log(nome);
-
-nome.shift(); // removendo elementos do início do vetor(array)
-console.log(nome);
-
-delete nome[0];
-console.log(nome); //removendo somente o valor do elemento, mas mantendo o índice do vetor(array)
-
-delete nome[2];
-console.log(nome);
+nome.push('Manuela'); // adicionando elementos ao final do vetor(array)
 console.log(nome);
