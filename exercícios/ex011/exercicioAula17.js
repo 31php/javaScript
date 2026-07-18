87af9ca0df93ef3df014b7f32f1c9974f3a8eb56
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-07-18 16:20:39 -0300

    treinando o preenchimento de vetores

--- exercícios/ex011/exercicioAula17.js
+++ exercícios/ex011/exercicioAula17.js
@@ -9,3 +9,15 @@ console.log(nome);
 
 nome.push('Manuela'); // adicionando elementos ao final do vetor(array)
 console.log(nome);
+
+nome.unshift('Arthur');
+console.log(nome); // adicionando elementos no início do vetor(array)
+
+nome.pop();
+console.log(nome); //excluindo elementos no final do vetor(array)
+
+nome.shift();
+console.log(nome); // excluindo elementos no início do vetor(array)
+
+delete nome[2];
+console.log(nome); // alterando somente o valor e deixando o espaço vazio
