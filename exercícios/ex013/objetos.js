94a1197094e5fc8b88236e5809ffec5c4da7cd0e
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-07-18 18:12:13 -0300

    treinando objetos em javascript

--- exercícios/ex013/objetos.js
+++ exercícios/ex013/objetos.js
@@ -1,15 +1,19 @@
 // objetos
 
-const pessoa = {
-    nome: 'phillip',
-    sobrenome: 'ferreira',
-    idade: 40
-}
-//console.log(pessoa.nome, pessoa.sobrenome, pessoa.idade);
+// const pessoa = {
+//     nome: 'phillip',
+//     sobrenome: 'ferreira',
+//     idade: 40
+// }
+// console.log(pessoa.nome, pessoa.sobrenome, pessoa.idade);
 
-function soma(a, b){
-    const soma = a + b;
-    return soma;
+function criaPessoa(nome, sobrenome, idade){
+    return { nome, sobrenome, idade };
 }
-soma(3, 4)
-console.log(soma);
+const pessoa1 = criaPessoa('phillip', 'gonçalves', 40);
+const pessoa2 = criaPessoa('joão', 'salvino', 60);
+const pessoa3 = criaPessoa('Maria', 'esmeralda', 42);
+const pessoa4 = criaPessoa('pedro', 'salazar', 22);
+const pessoa5 = criaPessoa('suzana', 'cerqueira', 30);
+
+console.log(pessoa1.nome,pessoa1.sobrenome,pessoa1.idade);
