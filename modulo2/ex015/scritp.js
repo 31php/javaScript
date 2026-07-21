70231226e76ddc456a471947f314e5f3528763c5
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-07-21 16:19:10 -0300

    treinando funções

---
+++ modulo2/ex015/scritp.js
@@ -0,0 +1,8 @@
+// exercícios básicos sobre função
+
+// function cumprimentar(nome){
+//     return (`olá ${nome}, seja bem-vindo ao mundo javaScript`)
+// }
+// console.log(cumprimentar('phillip'))
+// console.log(cumprimentar('paula'));
+// console.log(cumprimentar('Samira'));
