4148f4e8c64e6a54289ae34db9c8eebfc45330d6
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-07-23 16:16:49 -0300

    operadores logicos

---
+++ modulo3/ex001.js
@@ -0,0 +1,12 @@
+// Operadores lógicos
+
+/*
+&& -> and -> ê = todas as expressões precisam ser verdadeiras para retornar true
+|| -> or -> ou == basta um das expressões serem verdadeira para retornar true
+! -> not -> não = negação
+*/
+// const expressaoAnd = true && true && false;
+// console.log(expressaoAnd); 
+
+// const expressaoOr = true || true || false;
+// console.log(expressaoOr);
