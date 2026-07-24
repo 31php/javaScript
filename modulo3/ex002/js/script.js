6cfbfcd3e54c8c161332786156c1fc6ace4dbf14
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-07-24 18:45:15 -0300

    configurações javaScript iniciada

--- modulo3/ex002/js/script.js
+++ modulo3/ex002/js/script.js
@@ -1,1 +1,7 @@
 // capturar o evento de submit do formulário
+const form = window.document.querySelector('#formulario');
+
+form.addEventListener('submit', function(evento){
+    evento.preventDefault();
+    
+})
