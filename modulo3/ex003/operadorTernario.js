6d50a09f4b5e1f003c7ac0af14edd9874c4ee9dd
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-07-25 15:16:56 -0300

    exercício operadores ternários

---
+++ modulo3/ex003/operadorTernario.js
@@ -0,0 +1,40 @@
+// Operdor ternário
+
+// const idade = 15;
+// const situacao = idade <= 17 ? 'menor de idade' : 'maior de idade';
+// console.log(situacao);
+
+// let usuarioAutenticado = false;
+// let situacao = usuarioAutenticado ? 'logado' : 'deslogado';
+// console.log(situacao);
+
+// const valorCompra = 120;
+// const desconto = 20 /100;
+// const valorFinal = valorCompra - desconto;
+// console.log(valorFinal);
+
+
+//  const desconto = valorCompra >= 100 ? 20 : 5;
+//  console.log(desconto);
+
+// const checarParidade = (numero) => numero % 2 === 0 ? 'par' : 'impar';
+// console.log(checarParidade(10))
+// console.log(checarParidade(12));
+// console.log(checarParidade(25));
+
+// const temperatura = 32;
+// const statusClima = temperatura <= 15 ? 'frio' : (temperatura >= 29 ? 'calor':'agradavél');
+// console.log(statusClima);
+
+// const podeEditar = false;
+// const botaoTexto = podeEditar ? 'salvar alterações' : 'apenas visualizar';
+// console.log(botaoTexto);
+
+// const carrinhoDeCompras = 200;
+// const menbroPrime = true;
+// const freteGratis = carrinhoDeCompras >= 150 && menbroPrime === true ? 'frete gratis' : 'Frete padrão';
+// console.log(freteGratis);
+
+const obterSaudacao = (nome) => nome !== '' ? `olá ${nome}`: 'olá vistante';
+console.log(obterSaudacao('phillip'));
+console.log(obterSaudacao(''));
