e9194c660fff958ac3bc21b1976e157a30fa35d7
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-08-04 17:41:15 -0300

    treinando objeto date

---
+++ modulo3/ex004/objetoDate.js
@@ -0,0 +1,11 @@
+// trabalhando com o objeto Date
+const data = new Date();
+console.log('Dia', data.getDate());
+console.log('Ano', data.getFullYear());
+console.log('hora', data.getHours());
+console.log('Min', data.getMinutes());
+console.log('Seg', data.getSeconds());
+console.log('ms', data.getMilliseconds());
+console.log('Dia da semana', data.getDay());
+console.log(data.getTime());
+console.log(data.toString());
