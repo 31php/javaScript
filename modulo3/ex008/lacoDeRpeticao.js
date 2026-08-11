88e0dc134e37873e4fc9818817329d0b773e03a5
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-08-10 21:40:35 -0300

    laços de repetição(loop for)

---
+++ modulo3/ex008/lacoDeRpeticao.js
@@ -0,0 +1,13 @@
+// criando laços de repetição (for clássico)
+
+//1° criar uma variável de constrole atribuir um valor a ela
+
+// for (let i = 0; i <= 5; i = i + 1){
+//     console.log(i);
+// }
+
+const nomes = ['phillip', 'thaiane', 'manuela', 'arthur','marcia', 'maria julía'];
+
+for(let i = 0;i < nomes.length; i = i + 1){
+    console.log(nomes[i]);
+} 
