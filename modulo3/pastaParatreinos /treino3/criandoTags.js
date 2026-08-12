db0c322adc0491b8856e296d7579c3a252b4a511
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-08-12 19:11:47 -0300

    criando tags em html

--- modulo3/pastaParatreinos /treino3/criandoTags.js
+++ modulo3/pastaParatreinos /treino3/criandoTags.js
@@ -1,4 +1,8 @@
-// userindo tags hrml via javaScript
+// userindo tags html via javaScript
 
 const novaDiv = document.createElement('div');
 novaDiv.textContent = 'este elemento foi criado via javaScript';
+novaDiv.classList.add('');
+novaDiv.id = '';
+
+document.body.appendChild(novaDiv);
