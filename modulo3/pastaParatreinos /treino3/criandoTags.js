b059743bb7e2340f9e93d140e0390ceb51070e55
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-08-12 19:32:47 -0300

    continuação da criação de tags via js

--- modulo3/pastaParatreinos /treino3/criandoTags.js
+++ modulo3/pastaParatreinos /treino3/criandoTags.js
@@ -1,8 +1,15 @@
 // userindo tags html via javaScript
+const titulo = document.createElement('h1');
+titulo.textContent = 'Título de um documento';
+document.body.appendChild(titulo);
+
+const paragrafo = document.createElement('p');
+paragrafo.textContent = 'Um parágarfo para o texto criado via Js';
+
+document.body.appendChild(paragrafo);
+
 
 const novaDiv = document.createElement('div');
 novaDiv.textContent = 'este elemento foi criado via javaScript';
-novaDiv.classList.add('');
-novaDiv.id = '';
 
 document.body.appendChild(novaDiv);
