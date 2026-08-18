bd22e4ce51076bd2ab8c8722cd937ab5911d2377
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-08-18 17:28:03 -0300

    refazendo o exercicio

--- modulo3/ex009/js/script.js
+++ modulo3/ex009/js/script.js
@@ -1,18 +1,2 @@
-const elementos = [
-    {tag: 'p', texto: 'Frase 1'},
-    {tag: 'div', texto: 'Frase 2'},
-    {tag: 'section', texto: 'Frase 3'},
-    {tag: 'footer', texto: 'Frase 4'}
-];
 
-const caixa = window.document.querySelector('#container');
-const novaDiv = document.createElement('div');
-
-for(let i = 0; i < elementos.length; i = i + 1){
-    let {tag, texto} = elementos[i];
-    let tagCriada= document.createElement(tag);
-    tagCriada.innerText = texto;
-    novaDiv.appendChild(tagCriada);
-}
-caixa.appendChild(novaDiv);
 
