ee2412f9ca80824ae9816913006def50bbd0b7e1
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2026-08-23 14:04:46 -0300

    criação da mídia para telas pequenas

---
+++ modulo3/pastaParaTreinos /treino2/treino2B/js/script.js
@@ -0,0 +1,23 @@
+const form = window.document.querySelector('#meuFormulario');
+const res = window.document.querySelector('#resultado');
+const situacao = window.document.querySelector('#resposta');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    const pt = Number(window.document.querySelector('#n1').value);
+    const mt = Number(window.document.querySelector('#n2').value);
+    const ht = Number(window.document.querySelector('#n3').value);
+    const go = Number(window.document.querySelector('#n4').value);
+
+    const notas = pt + mt + ht + go;
+    const media = notas / 4;
+     
+    if(media < 7){
+        res.innerHTML = media;
+        situacao.innerHTML = 'Aprovado';
+    }else{
+        res.innerHTML = media;
+        situacao.innerHTML = 'Reprovado';
+    }
+})
